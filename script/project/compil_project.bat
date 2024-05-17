fbcdb4577dba6af3718192bb6a78e8491cf8b49f
John <john-wang@hotmail.fr>
2024-05-17 05:14:53 +0200

    script

--- script/project/compil_project.bat
+++ script/project/compil_project.bat
@@ -3,8 +3,8 @@ cd ../../prototype-ing1
 call mvn clean compile
 call mvn package
 call mvn install
-cd ihm-client
-call mvn exec:java
+cd demo194
+call mvn javafx:run
 
 if %errorlevel% equ 0 (
     echo Compilation went successfully.
